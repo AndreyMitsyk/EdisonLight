34fc6b6f46d18d79967ba2d8422934a24d21d642
AndreyMitsyk <mitzyk@mail.ru>
2016-01-05 22:44:45 +0300

    Initial commit of the ble control The light control via Edison ble (Bluetooth low energy).

--- main.js
+++ main.js
@@ -1,90 +1,67 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jslint node:true,vars:true,bitwise:true,unparam:true */
 
-// Initialize libraries.
-var mraa = require('mraa');
+/* rfkill unblock bluetooth
+ * killall bluetoothd
+ * hciconfig hci0 up
+ */
+
+// Using the bleno module
+var noble = require('noble');
+var display = require('intel-edison-lcd-rgb-backlight-display-helper');
 var groveSensor = require('jsupm_grove');
-var ms = require('microseconds');
-
-// Declare grove devices.
 var lightSensor = new groveSensor.GroveLight(0);
 var led = new groveSensor.GroveLed(8);
+var button = new groveSensor.GroveButton(5);
 
-// Configure ultrasonic sensor.
-var echoPin = new mraa.Gpio(6);
-var trigPin = new mraa.Gpio(7);
-trigPin.dir(mraa.DIR_OUT);
-echoPin.dir(mraa.DIR_IN);
+// Set display rows/cols
+display.set(2, 16);
+// Set backlight color from 2 colors and range
+display.setColorFromTwoColors('green', 'yellow', 0.5);
+display.write('Loading...');
 
-// Declare variables.
-var maximumRange = 400;
-var minimumRange = 4;
-var minimumLight = 2;
-var activateDistance = 70;
 var flag = false;
 
-// TODO: remove display after debug.
-var display = require('intel-edison-lcd-rgb-backlight-display-helper');
-display.set(2, 16);
-display.setColorFromTwoColors('green', 'yellow', 0.5);
-
-// Sets microseconds delay.
-function usleep(us) {
-    var start = ms.now();
-    while (true) {
-        if (ms.since(start) > us) {
-            return;
-        }
-    }
-}
-
-// Counts the distance with help of ultrasonic sensor.
-var ReadSensor = function() {
-    var pulseOn, pulseOff;
-    var duration, distance;
-    
-    trigPin.write(0);
-    usleep(2);
-    trigPin.write(1);
-    usleep(10);
-    trigPin.write(0);
-    
-    while (echoPin.read() === 0) {
-        pulseOff = ms.now();
-    }
-    while (echoPin.read() === 1) {
-        pulseOn = ms.now();
-    }
-    duration = pulseOn - pulseOff;
-    distance = parseInt(duration / 58.2);
-    
-    if (distance >= maximumRange || distance <= minimumRange) {
-        // TODO: remove display after debug.
-        display.write([lightSensor.value() + ' lux', 'Out of range']);
-    } else {
-        // TODO: remove display after debug.
-        display.write([lightSensor.value() + ' lux', distance + ' cm']);
-
-        CheckValues(lightSensor.value(), distance);
-    }
-};
-
-// Compares the values with the sensors with specified constants.
-function CheckValues(light, distance) {
-    if (light < minimumLight && distance <= activateDistance) {
-        LightOn();
-    }
-}
-
-// Activates the relay module.
-function LightOn() {
-    if (!flag) {
-        flag = true;
-        led.on();
-        setTimeout(function() {
-            led.off();
+noble.on('stateChange', function (state) {
+    if (state === 'poweredOn') {
+        noble.startScanning();
+        console.log('Started!');
+        display.write([null, 'Started...']);
+        var checkInterval = setInterval(function () {
+            if (button.value()) {
+                noble.stopScanning();
+                led.off();
+                flag = true;
+                display.write(['Stopped!','']);
+                clearInterval(checkInterval);
+            }
+            if (!flag) {
+                led.off();
+                console.log('Time out!');
+                display.write([null, 'Time out!']);
+            }
             flag = false;
-        }, 500);
+        }, 4500);
+    } else {
+        noble.stopScanning();
+        flag = true;
     }
-}
+});
 
-setInterval(ReadSensor, 60);
+noble.on('discover', function (peripheral) {
+    if (peripheral.advertisement.localName === 'MI') {
+        flag = true;
+        noble.stopScanning();
+        console.log('RSSI: ' + peripheral.rssi + '; ' + lightSensor.value() + ' lux');
+        display.write([lightSensor.value() + ' lux', 'RSSI: ' + peripheral.rssi]);
+        CheckValues(lightSensor.value(), peripheral.rssi);
+    }
+});
+
+function CheckValues(light, rssi) {
+    if (light < 2 & rssi > -88) {
+        led.on();
+    } else {
+        led.off();
+    }
+    noble.startScanning();
+}
